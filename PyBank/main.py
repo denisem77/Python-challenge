68456701f271093a78963b808b6fafae88aa0420
denisem77 <denisem8810@gmail.com>
2024-06-24 13:27:53 -0500

    Added the Average Change to the script

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,6 +8,9 @@ csvpath = os.path.join('Resources', 'budget_data.csv')
 unique_months = set()
 
 net_amount = 0
+previous_net = None
+changes= [] 
+total_months = 0
 
 with open(csvpath) as csvfile:
     pybank = csv.reader(csvfile, delimiter = ",")
@@ -17,13 +20,23 @@ with open(csvpath) as csvfile:
 
     for row in rows:
         date = row[0]
+        Earnings = int(row[1])
         unique_months.add(date)
-        net_amount += int(row[1])
+        net_amount += Earnings
+
+        if previous_net is not None:
+            change = Earnings - previous_net
+            changes.append(change)
+        
+        previous_net = Earnings
+        total_months += 1
       
-    total_months = len(unique_months 
-                         )
+    total_months = len(unique_months)
+    avg_change = sum(changes) / len(changes) if changes else 0
+
     print(f"Total Months: {total_months}")
     print(f"Total: ${net_amount}")
+    print(f"Average Change: ${avg_change: .2f}")
 
     
 
@@ -33,7 +46,7 @@ with open(csvpath) as csvfile:
     #    Title: Financial Analysis
     # ------------------------------
     # The total number of months included in the dataset:complete
-    # The net total amount of "Profit/Losses" over the entire period
+    # The net total amount of "Profit/Losses" over the entire period: complete
     # The changes in "Profit/Losses" over the entire period, and the the average of those changes
     # The greatest increase in profits (date and amount) over the entire period.
     # The greatest decrease in profits (date and amount) over the entire period.

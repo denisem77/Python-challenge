c862d0a4f8e72182acff31efbabe9e484c53093e
denisem77 <denisem8810@gmail.com>
2024-06-24 14:49:41 -0500

    Added expectations for the project

--- PyRoll/main.py
+++ PyRoll/main.py
@@ -11,3 +11,25 @@ with open(csvpath) as csvfile:
 
     for row in csvreader:
         print(row)
+
+
+
+
+#Create a script that analyzes the votes and calculates the following:
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+# The total number of votes each candidate won
+# The winner of the election based on popular vote
+
+# These are the results:
+# Election Results
+# -----------------
+# Total Votes: 369711
+# --------------------
+# Charles Casper Stockham: 23.049% (85213)
+# Diana DeGette: 73.812% (272892)
+# Raymon Anthony Doane: 3.139% (11606)
+# -----------------------------------------
+# Winner: Diana DeGette
+# ------------------------------------

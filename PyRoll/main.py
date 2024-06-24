5fac64bd5e59fcd7c03e762ae200d53bc6d8ce32
denisem77 <denisem8810@gmail.com>
2024-06-23 20:36:22 -0500

    Added the Script for importing the CSV file
     Changes to be committed:
    	modified:   PyBank/main.py
    	new file:   PyBank/main2.py.code-workspace
    	modified:   PyRoll/main.py
    Added the Script for importing the CSV file

---
+++ PyRoll/main.py
@@ -0,0 +1,13 @@
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+ 
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
+        print(row)

6a5334582c7b823dda9ee582148f55327cca4661
denisem77 <denisem8810@gmail.com>
2024-06-24 16:15:34 -0500

    Added the Total votes

--- PyRoll/main.py
+++ PyRoll/main.py
@@ -8,6 +8,8 @@ print("-------------------------")
 csvpath = os.path.join('Resources', 'election_data.csv')
 
 total_votes = 0
+voted_candidates = {}
+
 
 with open(csvpath) as csvfile:
     pypoll = csv.reader(csvfile, delimiter =",")
@@ -17,12 +19,21 @@ with open(csvpath) as csvfile:
 
     for row in rows:
         total_votes += 1
-    
+        candidates = row[2]
+
+        if candidates in voted_candidates:
+            voted_candidates[candidates] += 1
+        else:
+            voted_candidates[candidates] = 1
+
+
 print(f"Total Votes:  {total_votes}")
 print("------------------")
+for candidates, votes in voted_candidates.items():
+    print(f"{candidates}:  {votes} ")
 
 #Create a script that analyzes the votes and calculates the following:
-#The total number of votes cast
+#The total number of votes cast: Complete
 #A complete list of candidates who received votes
 #The percentage of votes each candidate won
 # The total number of votes each candidate won
